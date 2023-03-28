8f81caf428b73c13262dfe0f80e3392f7474789f
EvMaMis <evmamis13@gmail.com>
2023-03-28 12:42:23 +0300

    Compare lines and write to same.txt

--- main.py
+++ main.py
@@ -14,9 +14,28 @@ def getLinesMassive(file):
         linesMassive.append(line)
     return linesMassive
 
+def writeToFile(fileName, line):
+    with open(f"{fileName}.txt", 'r+') as file:
+        file.seek(0, 2)
+        file.write(line)
+
+def compareMassives(first, second):
+    for line in first:
+        for secondLine in second:
+            if line == secondLine:
+                print(line)
+                first.remove(line)
+                second.remove(secondLine)
+                writeToFile("same", line)
+        
+
 firstFile = openFile("firstFile")
 secondFile = openFile("secondFile")
-printLines(firstFile, "First File")
-printLines(secondFile, "Second File")
-firstMassive = getLinesMassive(firstFile)
 
+
+firstMassive = getLinesMassive(firstFile)
+secondMassive = getLinesMassive(secondFile)
+
+print(firstMassive)
+
+compareMassives(firstMassive, secondMassive)
