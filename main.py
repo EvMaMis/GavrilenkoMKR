793b8ff33fd3141d3431c9441b278654f4dd2c7d
EvMaMis <evmamis13@gmail.com>
2023-03-28 13:24:58 +0300

    flake8

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def openFile(name):
     try:
         file = open(f"{name}.txt", 'r')
     except Exception:
-        raise(Exception)
+        raise (Exception)
     return file
+
 
 def printLines(file, numberOfFile):
     print(f"======={numberOfFile}=======")
@@ -11,16 +12,19 @@ def printLines(file, numberOfFile):
         print(line)
     print("\n========================")
 
+
 def getLinesMassive(file):
     linesMassive = []
     for line in file:
         linesMassive.append(line)
     return linesMassive
 
+
 def writeToFile(fileName, line):
     with open(f"{fileName}.txt", 'r+') as file:
         file.seek(0, 2)
         file.write(line)
+
 
 def compareMassives(first, second):
     for line in first:
@@ -34,10 +38,12 @@ def compareMassives(first, second):
     for secondLine in second:
         writeToFile("diff", secondLine)
 
+
 def closeFile(name):
     with open(f"{name}.txt", 'w+') as f:
         f.seek(0)
         f.write("")
+
 
 closeFile("diff")
 closeFile("same")
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,15 +2,18 @@ import pytest
 
 import main
 
+
 @pytest.mark.parametrize("path", ["firstFile", "secondFile"])
 def test_get_right_open(path):
     file = main.openFile(path)
     assert file.name == f"{path}.txt"
 
+
 @pytest.mark.parametrize("path", ["af", "aasf"])
 def test_get_wrong_open(path):
     with pytest.raises(Exception):
         main.openFile(path)
+
 
 def test_get_massive():
     file = main.openFile("firstFile")
@@ -20,4 +23,3 @@ def test_get_massive():
     for line in file.readlines():
         assert line == massive[i]
         i += 1
-
